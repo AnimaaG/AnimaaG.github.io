7a77a0d46dff3abb4f7c4840bb3c89cdbcb2f122
AnimaaG <aniketghadi010@gmail.com>
2026-09-24 00:54:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11861144
+    "zoneId": 11872433
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11683915
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
